ab36096bb0f3f56c9aaced46c3e27ae5d1458e1e
mrmthornton <mrmthornton@gmail.com>
2019-03-02 15:35:57 -0500

    writing to clean dir

--- eda.py
+++ eda.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+import librosa
 from python_speech_features import mfcc, logfbank
 
 def plot_signals(signals):
@@ -36,7 +37,7 @@ def plot_fft(fft):
 
 def plot_fbank(fbank):
     fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False,
-                             sharey=True, figsize=(20,5))
+                             sharey=True, figsize=(20,3))
     fig.suptitle('Filter Bank Coefficients', size=16)
     i = 0
     for x in range(2):
@@ -50,7 +51,7 @@ def plot_fbank(fbank):
 
 def plot_mfccs(mfccs):
     fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False,
-                             sharey=True, figsize=(20,5))
+                             sharey=True, figsize=(20,2))
     fig.suptitle('Mel Frequency Cepstrum Coefficients', size=16)
     i = 0
     for x in range(2):
@@ -62,3 +63,79 @@ def plot_mfccs(mfccs):
             axes[x,y].get_yaxis().set_visible(False)
             i += 1
 
+def calc_fft(y, rate):
+    n = len(y)
+    freq = np.fft.rfftfreq(n, d=1/rate)
+    Y = abs(np.fft.rfft(y)/n)
+    return(Y, freq)
+    
+def envelople(y, rate, threshold):
+    mask = []
+    y = pd.Series(y).apply(np.abs)
+    y_mean = y.rolling(window=int(rate/10), min_periods=1, center=True).mean()
+    for mean in y_mean:
+        if mean > threshold:
+            mask.append(True)
+        else:
+            mask.append(False)
+    return mask   
+
+
+###############################################################################
+    
+df = pd.read_csv('instruments.csv')
+df.set_index('fname',inplace=True)
+for filename in df.index:
+    rate, signal = wavfile.read('wavfiles/'+filename)
+    df.at[filename,'length'] = signal.shape[0]/rate
+    
+labels = list(np.unique(df.label))
+class_dist = df.groupby(['label'])['length'].mean()
+
+fig, ax = plt.subplots()
+ax.set_title('Class Distibution', y=1.08)
+ax.pie(class_dist, labels=class_dist.index,
+       autopct='%1.1f%%', shadow=False, startangle=90)
+ax.axis('equal') # make chart aspect ration equal (cirle)
+plt.show()
+
+df.reset_index(inplace=True)
+
+signals = {}
+fft = {}
+fbank = {}
+mfccs = {}
+
+
+#fft
+for instrument in labels:
+    wav_file= df[df.label==instrument].iloc[0,0]
+    signal, rate = librosa.load ('wavfiles/'+wav_file, sr=44100)
+    mask = envelople(signal, rate, 0.0005)
+    signal = signal[mask]
+    signals[instrument] = signal
+    fft[instrument] = calc_fft(signal, rate)
+    
+    bank = logfbank(signal[:rate], rate, nfilt=26, nfft=1103).T
+    fbank[instrument] = bank
+    
+    mel = mfcc(signal[:rate], rate, numcep=13, nfilt=26, nfft=1103).T
+    mfccs[instrument] = mel
+    
+plot_signals(signals)
+plt.show
+
+plot_fft(fft)
+plt.show
+
+plot_fbank(fbank)
+plt.show
+
+plot_mfccs(mfccs)
+plt.show
+
+if len(os.listdir('clean')) == 0:
+    for f in tqdm(df.fname):
+        signal, rate = librosa.load('wavfiles/'+f, sr=16000)
+        mask = envelople(signal, rate, 0.0005)
+        wavfile.write(filename='clean/'+f, rate=rate, data=signal[mask])
